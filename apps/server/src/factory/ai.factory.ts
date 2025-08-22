2118e762e25b29d8155a97e9729d1a489bd1d5e7
sriharsha <sriharshar.471@gmail.com>
2025-08-22 14:08:15 +0530

    Refactoring the import

--- apps/server/src/factory/ai.factory.ts
+++ apps/server/src/factory/ai.factory.ts
@@ -1,11 +1,11 @@
 import { AnalysisResult } from "../models/ai.models";
 import { AiModel } from "../models/enum/models.enum";
+import {analyzeDocuments} from "../services/gemini.ai.service";
 
 export async function detetmineAiModel  ( cvText: string,
   jobDescriptionText: string, model: AiModel) : Promise<AnalysisResult>  {
 
     if(model === AiModel.GEMINI) {
-        const { analyzeDocuments } = await import('../services/gemini.ai.service');
         return analyzeDocuments(cvText, jobDescriptionText);
     }
     throw new Error(`Unsupported AI model: ${model}`);
